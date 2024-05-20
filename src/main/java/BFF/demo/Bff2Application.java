398157b3400f68c6641f09e4f71730f11166d771
maja rotovnik <maja.rotovnik001@gmail.com>
2024-05-20 18:46:28 +0200

    first

---
+++ src/main/java/BFF/demo/Bff2Application.java
@@ -0,0 +1,13 @@
+package BFF.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bff2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bff2Application.class, args);
+	}
+
+}
---
+++ src/test/java/BFF/demo/Bff2ApplicationTests.java
@@ -0,0 +1,13 @@
+package BFF.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bff2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
